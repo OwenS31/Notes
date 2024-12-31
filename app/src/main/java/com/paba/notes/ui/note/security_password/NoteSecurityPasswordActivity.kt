cafbd9aa5993705c9a2188d097fd0da31e5ddf99
Owen Samuel <owensamuel31@gmail.com>
2024-12-31 10:15:25 +0700

    note security + fun navigateToNoteSecurity

--- app/src/main/java/com/paba/notes/ui/note/security_password/NoteSecurityPasswordActivity.kt
+++ app/src/main/java/com/paba/notes/ui/note/security_password/NoteSecurityPasswordActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.paba.notes.R
 import com.paba.notes.databinding.ActivitySecurityPasswordBinding
 import com.paba.notes.ui.note.edit.EditNoteActivity.Companion.EXTRA_NOTE_ID
 
@@ -43,6 +44,46 @@ class NoteSecurityPasswordActivity : AppCompatActivity() {
     }
     private fun initView() = binding.apply {
         btnLogout.isVisible = false
-
+        btnVerify.setOnClickListener { validateInput() }
     }
+    private fun validateInput() = binding.apply {
+
+        showLoading(true)
+
+        inputSecurityPassword.error = null
+
+        val securityPassword = inputSecurityPassword.editText?.text.toString().trim()
+
+        if (securityPassword.isBlank()) {
+
+            inputSecurityPassword.error =
+                getString(R.string.error_empty_field, getString(R.string.label_security_password))
+            showLoading(false)
+        } else {
+
+
+        }
+    }
+    private fun showLoading(isShowLoading: Boolean) = binding.apply {
+        inputSecurityPassword.isEnabled = !isShowLoading
+
+        btnVerify.apply {
+            isEnabled = !isShowLoading
+            text = if (isShowLoading) {
+                // Show verifying text when loading
+                getString(R.string.action_verifying)
+            } else {
+                // Show verify text when not loading
+                getString(R.string.action_login)
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Key for passing the note ID as an argument.
+         */
+        const val EXTRA_NOTE_ID = "extra_note_id"
+    }
+
 }
